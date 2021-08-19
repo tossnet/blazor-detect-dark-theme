f076634adafccbcec445acd4b095f9e50b2e923f
Christophe Peugnet <christophe.peugnet@outlook.fr>
2021-08-19 08:36:38 +0200

    .

---
+++ BlazorDetectTheme/BlazorDetectTheme/Services/BrowserModeService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorDetectTheme.Services
+{
+    public class BrowserModeService
+    {
+        private readonly IJSRuntime _js;
+
+        public BrowserModeService(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task<bool> IsDarkMode()
+        {
+            return await _js.InvokeAsync<bool>("IsDarkMode");
+        }
+    }
+}
---
+++ BlazorDetectTheme/BlazorDetectTheme/wwwroot/js/browserMode.js
@@ -0,0 +1,8 @@
+﻿function IsDarkMode() {
+    let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (matched)
+        return true;
+    else
+        return false;
+}
